4c4eba1c213515a8a264f6ebccf2bcb2fc896172
Даниил Мишарин <danila-master@inbox.ru>
2022-04-20 17:09:01 +0400

    v_1_1

--- search-server/paginator.h
+++ search-server/paginator.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iterator>
 #include <ostream>
+#include <cassert>
 
 template <typename Iterator>
 class IteratorRange {
@@ -34,6 +35,7 @@ template <typename Iterator>
 class Paginator {
 public:
     Paginator(Iterator begin, Iterator end, size_t page_size) {
+        std::assert(end >= begin && page_size > 0);
         for (size_t left = std::distance(begin, end); left > 0;) {
             const size_t current_page_size = std::min(page_size, left);
             const Iterator current_page_end = std::next(begin, current_page_size);
